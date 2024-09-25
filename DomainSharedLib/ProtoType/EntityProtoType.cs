8541a88d722a12a45a16855adfdf44a145c843ef
rubensmanhaesb <rubensmanhaesb@hotmail.com>
2024-09-25 00:18:29 -0300

    EntityProtoType - Refatorando a classe para fazer cópia profunda de listas

--- DomainSharedLib/ProtoType/EntityProtoType.cs
+++ DomainSharedLib/ProtoType/EntityProtoType.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Collections;
 
 namespace DomainSharedLib.ProtoType
 {
@@ -6,10 +6,40 @@ namespace DomainSharedLib.ProtoType
     {
         public static T CopyProperties<T>(T source) where T : new()
         {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
             T copy = new T();
             foreach (var prop in typeof(T).GetProperties().Where(p => p.CanWrite))
             {
-                prop.SetValue(copy, prop.GetValue(source, null), null);
+                var value = prop.GetValue(source, null);
+
+                if (value is IEnumerable<object> enumerable && !(value is string))
+                {
+                    // Criar uma nova lista para o IEnumerable, copiando os itens de forma profunda
+                    var listType = typeof(List<>).MakeGenericType(prop.PropertyType.GetGenericArguments()[0]);
+                    var copyList = (IList)Activator.CreateInstance(listType);
+
+                    foreach (var item in enumerable)
+                    {
+                        // Fazer cópia profunda de cada item na lista
+                        var deepCopyItem = CopyProperties(item);
+                        copyList.Add(deepCopyItem);
+                    }
+
+                    prop.SetValue(copy, copyList, null);
+                }
+                else if (value != null && !value.GetType().IsValueType && value.GetType() != typeof(string))
+                {
+                    // Se a propriedade for um objeto complexo, realizar a cópia profunda recursivamente
+                    var deepCopy = CopyProperties(value);
+                    prop.SetValue(copy, deepCopy, null);
+                }
+                else
+                {
+                    // Para tipos de valor e strings, apenas copie diretamente
+                    prop.SetValue(copy, value, null);
+                }
             }
             return copy;
         }
